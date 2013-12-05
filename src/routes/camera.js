7bd146fc512f91df6d3c3c4b5dede4c0300c4e6c
MFoster <mattfoster01@gmail.com>
2013-12-05 00:22:43 +0000

    Removed debounce time from the list of options

--- src/routes/camera.js
+++ src/routes/camera.js
@@ -2,9 +2,9 @@ var RaspiCam = require("raspicam");
 var debounce = require("debounce");
 var cameraConfig = {
 	mode : "photo",
-	timelapse : 100,
-	debounceTime : 500
+	timelapse : 100
 };
+var debounceTime = 500;
 
 var cameraFilePath = __dirname + "/../public/images/camera/photo/";
 
@@ -12,7 +12,7 @@ exports.photo = function(request, response){
 	var name = new Date().getTime() + ".jpg";
 	cameraConfig.output = cameraFilePath + name;
 	var camera = new RaspiCam(cameraConfig);
-	camera.on("change", debounce(camera.stop, cameraConfig.debounceTime));
+	camera.on("change", debounce(camera.stop, debounceTime));
 	camera.on("exit", function(){
 		response.send({ message : "Successfully captured image", name : name });
 	});
